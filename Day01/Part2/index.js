d32d6bef657c34ca01e0bc5914b6ad43f8b95598
Ushie <ushiekane@gmail.com>
2023-12-02 20:05:15 +0300

    refactor(Day01 - Part2): clean up solution

--- Day01/Part2/index.js
+++ Day01/Part2/index.js
@@ -1,61 +1,41 @@
 const list = ["two1nine", "eightwothree", "abcone2threexyz", "xtwone3four", "4nineeightseven2", "zoneight234", "7pqrstsixteen"]
 
-const wordAndDigit = Object.entries({
-    one: 1,
-    two: 2,
-    three: 3,
-    four: 4,
-    five: 5,
-    six: 6,
-    seven: 7,
-    eight: 8,
-    nine: 9
-})
+const wordToDigit = { one: 1, two: 2, three: 3, four: 4, five: 5, six: 6, seven: 7, eight: 8, nine: 9 }
 
-function insert(str, substring, index) {
+function insertAtIndex(str, substring, index) {
     const arr = str.split('');
-
     arr.splice(index, 0, substring);
-
     return arr.join('');
 }
 
-const transformedList = list.map((item) => {
-    let hasWords = true;
+function transformWord(word) {
+    let remainingEntries = true;
 
-    while (hasWords == true) {
-        function getValidEntriesAndIndexes() {
-            let objectOfFoundWordsAndIndexes = [];
-            wordAndDigit.forEach((digit) => {
-                const [key, value] = digit;
-                const index = item.indexOf(key);
-                if (index != -1) {
-                    const secondIndex = index + key.length;
+    while (remainingEntries) {
+        const foundEntries = Object.entries(wordToDigit).map(([key, value]) => {
+            const index = word.indexOf(key);
+            if (index !== -1) {
+                const wordEndIndex = index + key.length;
+                return { index: wordEndIndex, word: key, replacement: value };
+            }
+            return null;
+        }).filter(Boolean);
 
-                    objectOfFoundWordsAndIndexes[index] = {
-                        index: secondIndex,
-                        word: key,
-                        replacement: value
-                    }
-                }
-            })
-            return objectOfFoundWordsAndIndexes.filter(Boolean);
-        }
-
-        const entries = getValidEntriesAndIndexes()
-
-        if (entries.length == 0) {
-            hasWords = false;
+        if (foundEntries.length === 0) {
+            remainingEntries = false;
         } else {
-            const entry = entries.at(0)
-            item = insert(item, entry.replacement, entry.index - 1)
+            const entry = foundEntries[0];
+            word = insertAtIndex(word, entry.replacement, entry.index - 1);
         }
     }
 
-    return item;
-});
+    return word;
+}
 
-const listOfNumbers = transformedList.map((item) => item.match(/\d/g)).map((numbers) => Number(numbers[0] + numbers.pop()));
+const listOfNumbers = list
+    .map(transformWord)
+    .map((item) => item.match(/\d/g))
+    .map((numbers) => Number(numbers[0] + numbers.pop()));
 const sum = listOfNumbers.reduce((acc, num) => acc + num);
 
 console.log(sum);
